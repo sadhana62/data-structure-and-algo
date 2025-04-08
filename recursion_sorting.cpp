a7eae0e1f74811cc8f48449309c459733455cce5
sadhana62 <sadhana.19582@sscbs.du.ac.in>
2025-04-08 20:12:25 +0530

    added-sorting-using-recurssion

--- recursion_sorting.cpp
+++ recursion_sorting.cpp
@@ -1,42 +1,47 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void insert(vector<int>& v,int temp){
-    if(v.size()==0 || v[v.size()-1]<= temp){
+void insert(vector<int> &v, int temp)
+{
+    if (v.size() == 0 || v[v.size() - 1] <= temp)
+    {
         v.push_back(temp);
         return;
     }
-    int top = v[v.size()-1];
+    int top = v[v.size() - 1];
     v.pop_back();
-    insert(v,temp);
+    insert(v, temp);
     v.push_back(top);
-    
 }
 
-void sort(vector<int>&v){
-    if(v.size()==1|| v.size()==0) {
+void sort(vector<int> &v)
+{
+    if (v.size() == 1 || v.size() == 0)
+    {
         return;
     }
-    int top = v[v.size()-1];
+    int top = v[v.size() - 1];
     v.pop_back();
     sort(v);
-    insert(v,top);
-
+    insert(v, top);
 }
 
-int main(){
-//   this program sorting using recurssion
+int main()
+{
+    //   this program sorting using recurssion
 
-   vector <int> v={4,3,2,6,7,1,3};
-   cout<<"Before sorting\n";
-   for(int i =0;i<v.size();i++){
-    cout<<v[i]<<" ";
-   }
-   sort(v);
-   cout<<"\nAfter sorting\n";
-   for(int i =0;i<v.size();i++){
-    cout<<v[i]<<" ";
-   }
-   return 0;
+    vector<int> v = {4, 3, 2, 6, 7, 1, 3};
+    cout << "Before sorting\n";
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    sort(v);
+    cout << "\nAfter sorting\n";
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    return 0;
 }
