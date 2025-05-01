066ddf1be977435298ce2b0031ba434c0996a581
sadhana62 <i.sadhana2024@gmail.com>
2025-05-01 12:53:41 +0530

    added few more permutation type

--- recurssion/permlowerabc.cpp
+++ recurssion/permlowerabc.cpp
@@ -1,5 +1,32 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
+void solve2(string i, string o,vector<string>&v){
+
+    if (i.length()==0){
+        v.push_back(o);
+        return;
+    }
+
+    if (isalpha(i[0])){
+        string op1 = o;
+        string op2 = o;
+        op1.push_back(tolower(i[0]));
+        op2.push_back(toupper(i[0]));
+        i.erase(i.begin()+0);
+        solve2(i,op1,v);
+        solve2(i,op2,v);
+        return;
+    } else {
+        string op = o;
+        op.push_back(i[0]);
+        i.erase(i.begin()+0);
+        solve2(i,op,v);
+        return;
+    }
+
+}
 
 void solve(string inp,string op){
     if (inp.length()==0){
@@ -18,11 +45,28 @@ void solve(string inp,string op){
 
 int main(){
     /*
-      ab = Ab AB aB ab
+      ab = Ab AB aB ab this is for smaller case letters
     */
     string inp;
     cout<<"Enter the string in small cases\n";
     cin>>inp;
     string op = "";
     solve(inp,op);
+
+    cout<<"\nNow we can take digits or smaller case or upper case letters as well \n";
+    /*
+      Ab1c  = ab1c Ab1C  AB1c AB1C aB1c ...
+    */
+
+    string i;
+    cout<<"Enter the string\n";
+    cin>>i;
+    vector<string>v;
+    string o ="";
+    solve2(i,o,v);
+    cout<<"\n And the required permutation is\n";
+    for (auto i:v){
+        cout<<i<<" ";
+    }
+    return 0;
 }
