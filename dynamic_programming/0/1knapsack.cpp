7067510945c2aec1cd2864a1dc5730f608645dfe
sadhana62 <i.sadhana2024@gmail.com>
2025-05-28 00:03:47 +0530

    used memoization 0-1

--- dynamic_programming/0/1knapsack.cpp
+++ dynamic_programming/0/1knapsack.cpp
@@ -2,20 +2,36 @@
 #include<vector>
 using namespace std;
 
+// Define the memoization table as a global variable
+vector<vector<int>> mem;
+
+
+
 int knapsack(vector<int>&weights, vector<int>&items, int tot, int W){
+    // Memoization table should not be re-initialized on every call; move it outside the function for correct memoization.
     if (tot == 0 || W == 0){
        return 0;
     }
     if (weights[tot-1] <= W){
-        return max(items[tot-1] + knapsack(weights, items, tot-1, W - weights[tot-1]),knapsack(weights, items, tot-1, W));
+        if (mem[tot][tot] != -1){
+            return mem[tot-1][tot-1];
+        } else {
+
+            return mem[tot-1][tot-1] = max(items[tot-1] + knapsack(weights, items, tot-1, W - weights[tot-1]),knapsack(weights, items, tot-1, W));
+        }
     }
     else {
-      return  knapsack(weights, items, tot-1, W);
+         if (mem[tot][tot] != -1){
+            return mem[tot-1][tot-1];
+        }else {
+            return mem[tot-1][tot-1]= knapsack(weights, items, tot-1, W);
+
+        }
     }
 }
 
 int main(){
-
+    
     vector<int> weights;
     int totNumItem;
     cout<<"Enter the number of items\n";
@@ -32,6 +48,10 @@ int main(){
     cout<<"Enter the maximum capacity of bag\n";
     int W;
     cin>>W;
+
+    // Initialize memoization table with -1
+    mem.assign(totNumItem+1, vector<int>(W+1, -1));
+   
     int profit = knapsack(weights,items,totNumItem,W);
      for(int i =0;i<totNumItem;i++){
        cout<<"W "<<weights[i]<<" "<<"I "<<items[i];
